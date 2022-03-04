c0c61e852e4ee211074fcd7a9e82eb98b8bf3f6f
Jovian Lim <kiriketsuki@gmail.com>
2022-03-04 18:57:30 +0800

    lesson 14

--- Lesson 14/main.js
+++ Lesson 14/main.js
@@ -1,6 +1,160 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+/**
+ * Base
+ */
+// Debug
+// const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+import matcapURL from './static/textures/matcaps/8.png';
+const matcapTexture = textureLoader.load(matcapURL);
+
+/**
+ * Font
+*/
+
+const fontLoader = new FontLoader();
+import fontURL from './static/fonts/helvetiker_regular.typeface.json';
+const font = fontLoader.parse(fontURL);
+function parseFont(font) {
+    // Create a text geometry
+    const textGeometry = new TextGeometry(
+        'Kiriketsuki',
+        {
+            font: font,
+            size: 0.5,
+            height: 0.2,
+            curveSegments: 4,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 4
+        }
+    );
+    textGeometry.center();
+
+    console.log(textGeometry.boundingBox);
+
+    // Create a text material
+    const textMaterial = new THREE.MeshMatcapMaterial({
+        matcap: matcapTexture
+    });
+    const text = new THREE.Mesh(
+        textGeometry,
+        textMaterial
+    );
+    scene.add(text);
+};
+
+parseFont(font);
+
+/**
+ * Objects
+*/
+const donutGeo = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+const donutMat = new THREE.MeshMatcapMaterial({
+    matcap: matcapTexture
+});
+
+for (var i = 0; i < 1000; i++) {
+    var donut = new THREE.Mesh(
+        donutGeo,
+        donutMat
+    );
+    donut.position.x = Math.random() * 20 - 10;
+    donut.position.y = Math.random() * 20 - 10;
+    donut.position.z = Math.random() * 20 - 10;
+
+    donut.rotation.x = Math.sin(Math.random() * Math.PI);
+    donut.rotation.y = Math.cos(Math.random() * Math.PI);
+
+    var scale = Math.random() * 0.5;
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+}
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+function loop() {
+    const elapsedTime = clock.getElapsedTime()
+    console.log("here");
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(loop)
+}
+
+loop()
